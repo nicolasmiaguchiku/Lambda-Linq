ca3281e66f6a51f07886004636290c39a831bbd6
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2024-06-24 20:15:16 -0300

    exercicio linq e lambda

--- Program.cs
+++ Program.cs
@@ -2,24 +2,26 @@
 using Linq.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
 
 
 namespace Linq
 {
-  class Program
-  {
-    static void Printer<T>(string Message, IEnumerable<T> obj)
+    class Program
     {
-      Console.WriteLine(Message);
-      foreach (T p in obj)
-      {
-        Console.WriteLine(p);
-      }
-    }
+        static void Printer<T>(string Message, IEnumerable<T> obj)
+        {
+            Console.WriteLine(Message);
+            foreach (T p in obj)
+            {
+                Console.WriteLine(p);
+                Console.WriteLine();
+            }
+        }
 
-    public static void Main()
-    {
-      List<Empoyees> list = new List<Empoyees>
+        public static void Main()
+        {
+            List<Empoyees> list = new List<Empoyees>
       {
         new Empoyees {Name = "Nicolas", Salary = 3000.00, Seniority = "Junior", SoftwareEngineering = "BackEnd" },
         new Empoyees {Name = "João", Salary = 3500.00, Seniority = "Junior", SoftwareEngineering = "Mobile"},
@@ -33,15 +35,27 @@ namespace Linq
         new Empoyees {Name = "Marcelo", Salary = 13500.00, Seniority = "Senior", SoftwareEngineering = "BackEnd"},
       };
 
-      var FrontEnd = list.Where(e => e.SoftwareEngineering == "FrontEnd");
-      Printer("Desenvovedores FrontEnd: ", FrontEnd);
+            var FrontEnd = list.Where(e => e.SoftwareEngineering == "FrontEnd");
+            Printer("Desenvovedores FrontEnd: ", FrontEnd);
 
-      System.Console.WriteLine();
-      System.Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
-      var BackEnd = list.Where(e => e.SoftwareEngineering == "BackEnd");
-      Printer("Desenvolvedores BackEnd:", BackEnd);
+            var BackEnd = list.Where(e => e.SoftwareEngineering == "BackEnd");
+            Printer("Desenvolvedores BackEnd:", BackEnd);
+
+            var FullStack = list.Where(e => e.SoftwareEngineering == "FullStack");
+            Printer("Desenvolvedores FullStack:", FullStack);
+
+            var Mobile = list.Where(e => e.SoftwareEngineering == "Mobile");
+            Printer("Desenvolvedores FullStack:", Mobile);
+
+            var DataAnalyst = list.Where(e => e.SoftwareEngineering == "DataAnalyst");
+            Printer("Desenvolvedores FullStack:", DataAnalyst);
+
+            var MediaSalarioJunior = list.Where(e => e.Seniority == "Junior").Select(e => e.Salary).Average();
+            Console.WriteLine("Media Salario Dos junior: " + MediaSalarioJunior.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
     }
-
-  }
 }
